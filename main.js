7d42a7a9ed2cdf4fb673c021d491faff6d6e3574
Santiago Yara <sanyara2010@gmail.com>
2023-08-31 14:24:13 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+let button1 = document.querySelector(".w1");
+let button2 = document.querySelector(".w2");
+let button3 = document.querySelector(".w3");
+let button4 = document.querySelector(".w4");
+
+function pestania1() {
+    document.getElementById("img1").style.height="93vh";
+    document.getElementById("img2").style.height="0px";
+    document.getElementById("img3").style.height="0px";
+    document.getElementById("img4").style.height="0px";
+}
+function pestania2() {
+    document.getElementById("img1").style.height="0px";
+    document.getElementById("img2").style.height="93vh";
+    document.getElementById("img3").style.height="0px";
+    document.getElementById("img4").style.height="0px";
+}
+function pestania4() {
+    document.getElementById("img1").style.height="0px";
+    document.getElementById("img2").style.height="0px";
+    document.getElementById("img3").style.height="93vh";
+    document.getElementById("img4").style.height="0px";
+}
+function pestania3() {
+    document.getElementById("img1").style.height="0px";
+    document.getElementById("img2").style.height="0px";
+    document.getElementById("img3").style.height="0px";
+    document.getElementById("img4").style.height="93vh";
+}
+button1.onclick = pestania1;
+button2.onclick = pestania2;
+button3.onclick = pestania3;
+button4.onclick = pestania4;
