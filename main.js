6f0fbfc24f1c8b0fe7918ef7d70bd022a833f145
Santiago Yara <sanyara2010@gmail.com>
2023-08-31 23:07:40 -0500

    last

--- main.js
+++ main.js
@@ -2,32 +2,47 @@ let button1 = document.querySelector(".w1");
 let button2 = document.querySelector(".w2");
 let button3 = document.querySelector(".w3");
 let button4 = document.querySelector(".w4");
+let button5 = document.querySelector(".w5");
+let button6 = document.querySelector(".back");
 
 function pestania1() {
     document.getElementById("img1").style.height="93vh";
     document.getElementById("img2").style.height="0px";
     document.getElementById("img3").style.height="0px";
     document.getElementById("img4").style.height="0px";
+    document.getElementById("img5").style.height="0px";
 }
 function pestania2() {
     document.getElementById("img1").style.height="0px";
     document.getElementById("img2").style.height="93vh";
     document.getElementById("img3").style.height="0px";
     document.getElementById("img4").style.height="0px";
+    document.getElementById("img5").style.height="0px";
 }
 function pestania4() {
     document.getElementById("img1").style.height="0px";
     document.getElementById("img2").style.height="0px";
     document.getElementById("img3").style.height="93vh";
     document.getElementById("img4").style.height="0px";
+    document.getElementById("img5").style.height="0px";
 }
 function pestania3() {
     document.getElementById("img1").style.height="0px";
     document.getElementById("img2").style.height="0px";
     document.getElementById("img3").style.height="0px";
     document.getElementById("img4").style.height="93vh";
+    document.getElementById("img5").style.height="0px";
+}
+function pestania5() {
+    document.getElementById("img1").style.height="0px";
+    document.getElementById("img2").style.height="0px";
+    document.getElementById("img3").style.height="0px";
+    document.getElementById("img4").style.height="0px";
+    document.getElementById("img5").style.height="93vh";
 }
 button1.onclick = pestania1;
 button2.onclick = pestania2;
 button3.onclick = pestania3;
 button4.onclick = pestania4;
+button5.onclick = pestania5;
+button6.onclick = pestania1;
